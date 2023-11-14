e00226aa472e259f5580ff88b529275af62fb86b
Yahya Tarique <coolyahya.03@gmail.com>
2023-11-14 14:20:27 +0530

    GSAP config done.

--- build/main.js
+++ build/main.js
@@ -11,14 +11,32 @@ function createHeroTimeline(tl) {
 }
 
 function createAchievementTimeline(tl) {
-    tl.from('.achievement', { y: 50, opacity: 0, duration: 0.5, stagger: 0.1, ease: "power1.out" })
-    tl.from('.achievement-card', { y: 50, opacity: 0, duration: 0.5, stagger: 0.1, ease: "power1.out" })
+    tl.from('.achievement', { y: 50, opacity: 0, duration: "-=0.5", stagger: 0.1, ease: "power1.out", scrollTrigger: {
+        trigger: ".hero",
+        start: "bottom top",
+        end: "bottom bottom",
+        markers: true,
+    }})
+    ScrollTrigger.create({
+        trigger: ".achievement",
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        pinSpacing: false,
+    })
+    tl.from('.achievement-card', { y: 50, opacity: 0, duration: "-=0.5", stagger: 0.1, ease: "power1.out" })
 }
-
-
 
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0 } });
 createHeaderTimeline(tl);
 createHeroTimeline(tl);
+// Pin "pin-1" while achievement is scrolled
+ScrollTrigger.create({
+    trigger: ".pin-1",
+    start: "top top",
+    end: "bottom top",
+    pin: ".pin-1",
+    snap: 2,
+});
 createAchievementTimeline(tl);
