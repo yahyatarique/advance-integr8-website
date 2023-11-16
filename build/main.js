79649d15d1fbad6ae8f4976ddfdce4514c8e52b5
Yahya Tarique <coolyahya.03@gmail.com>
2023-11-16 08:54:46 +0530

    Added About Us.

--- build/main.js
+++ build/main.js
@@ -24,15 +24,15 @@ function createHeroTimeline(tl) {
 }
 
 function createAchievementTimeline(tl) {
-  tl.from(".achievement", {
-    y: 50,
+  tl.from(".achievements", {
+    y: 250,
     opacity: 0,
-    // duration: "-=0.5",
+    duration: 0.4,
     stagger: 0.1,
-    ease: "power1.out",
+    ease: "power1.out"
   });
   tl.from(".achievement-card", {
-    y: 50,
+    y: 500,
     opacity: 0,
     // duration: "-=0.5",
     stagger: 0.1,
@@ -41,16 +41,64 @@ function createAchievementTimeline(tl) {
 }
 
 const tl = gsap.timeline({
-  defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0 },
+  defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0.1 },
 });
 createHeaderTimeline(tl);
 createHeroTimeline(tl);
 // Pin "pin-1" while achievement is scrolled
-// ScrollTrigger.create({
-//   trigger: ".pin-1",
-//   start: "top top",
-//   end: "bottom top",
-//   pin: ".pin-1",
-//   snap: 2,
-// });
+ScrollTrigger.create({
+  trigger: ".pin-1",
+  start: "top top",
+  end: "bottom bottom",
+  pin: ".pin-1",
+});
 createAchievementTimeline(tl);
+
+
+gsap.from('.blob', {
+  scrollTrigger: {
+    trigger: '.achievements',
+    start: 'top bottom',
+  },
+  duration: 6,
+  opacity: 0.25,
+  scale: 0.5,
+  repeat: 1,
+  // Provide different x and y for each blob
+  x: (a) => {
+    console.log(a, gsap.utils.random(-100, 100));
+    return gsap.utils.random(a > 0 ? -a * 100: 0, a > 0 ? 0 : a * 100);
+  },
+  y: (a) => {
+    return gsap.utils.random(-100, 100);
+  },
+  ease: 'power1.in',
+  stagger: 0.1
+})
+
+
+
+// function createCounter(htmlEl, count) {
+//   htmlEl.innerText = 0;
+//   gsap.to(htmlEl, {
+//     duration: 2,
+//     ease: 'power1.in',
+//     textContent: count,
+//     snap: {
+//       textContent: 1,
+//     },
+//     scrollTrigger: {
+//       trigger: '.achievements',
+//       start: 'top bottom',
+//       end: 'bottom top',
+//     }
+//   })
+// }
+
+// const achievement_1 = document.querySelector('#achievement_1');
+// const achievement_2 = document.querySelector('#achievement_2');
+// const achievement_3 = document.querySelector('#achievement_3');
+
+// createCounter(achievement_1, achievement_1.innerText);
+// createCounter(achievement_2, achievement_2.innerText);
+// createCounter(achievement_3, achievement_3.innerText);
