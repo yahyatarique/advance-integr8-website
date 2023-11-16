9ef27387b3252ee8c16b9f624d5e3a774c2d7e02
Yahya Tarique <coolyahya.03@gmail.com>
2023-11-16 11:54:57 +0530

    Added Animations.

--- build/main.js
+++ build/main.js
@@ -34,47 +34,62 @@ function createAchievementTimeline(tl) {
   tl.from(".achievement-card", {
     y: 500,
     opacity: 0,
-    // duration: "-=0.5",
     stagger: 0.1,
     ease: "power1.out",
   });
 }
 
+function createAboutUsTimeline(tl) {
+  tl.from(".about-us-header", {
+    y: 250,
+    scale: 1.5,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "power1.out",
+  });
+  tl.from(".col", {
+    y: 500,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "power1.out",
+  });
+}
+
 const tl = gsap.timeline({
-  defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0.1 },
+  defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0.2 },
 });
 createHeaderTimeline(tl);
 createHeroTimeline(tl);
 // Pin "pin-1" while achievement is scrolled
-ScrollTrigger.create({
-  trigger: ".pin-1",
-  start: "top top",
-  end: "bottom bottom",
-  pin: ".pin-1",
-});
+// ScrollTrigger.create({
+//   trigger: ".pin-1",
+//   start: "top top",
+//   end: "bottom bottom",
+//   pin: ".pin-1",
+// });
 createAchievementTimeline(tl);
+createAboutUsTimeline(tl);
 
 
-gsap.from('.blob', {
-  scrollTrigger: {
-    trigger: '.achievements',
-    start: 'top bottom',
-  },
-  duration: 6,
-  opacity: 0.25,
-  scale: 0.5,
-  repeat: 1,
-  // Provide different x and y for each blob
-  x: (a) => {
-    console.log(a, gsap.utils.random(-100, 100));
-    return gsap.utils.random(a > 0 ? -a * 100: 0, a > 0 ? 0 : a * 100);
-  },
-  y: (a) => {
-    return gsap.utils.random(-100, 100);
-  },
-  ease: 'power1.in',
-  stagger: 0.1
-})
+// gsap.from('.blob', {
+//   scrollTrigger: {
+//     trigger: '.achievements',
+//     start: 'top bottom',
+//   },
+//   duration: 2,
+//   opacity: 0.25,
+//   scale: 0.5,
+//   repeat: 1,
+//   x: (a) => {
+//     console.log(a, gsap.utils.random(-100, 100));
+//     return gsap.utils.random(a > 0 ? -a * 100: 0, a > 0 ? 0 : a * 100);
+//   },
+//   y: (a) => {
+//     return gsap.utils.random(-100, 100);
+//   },
+//   ease: 'power1.in',
+//   stagger: 0.2
+// })
 
 
 
