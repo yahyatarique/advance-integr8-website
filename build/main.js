1ae80682f2f7e586c7fd4d786f6bd37e5b443947
Yahya Tarique <coolyahya.03@gmail.com>
2023-11-16 22:36:04 +0530

    Added Scrolls.

--- build/main.js
+++ build/main.js
@@ -1,53 +1,63 @@
 gsap.registerPlugin(ScrollTrigger);
 
 function createHeaderTimeline(tl) {
-  tl.from(".logo", { y: -50, x: -100, opacity: 0, scale: 0 });
-  tl.from(".nav-item", { y: -50, opacity: 0, stagger: 0.1 });
+  tl.from(".logo", { y: -50, x: -100, opacity: 0, scale: 0, ease: "power1.out", duration: 0.5 });
+  tl.from(".nav-item", { y: -50, opacity: 0, stagger: 0.1, ease: "power1.out", duration: 0.5 });
 }
 
 function createHeroTimeline(tl) {
-  tl.from(".hero-section .btn", {
-    scale: 1.75,
-    y: -100,
+  tl.from(".hero-item", {
+    y: 20,
     opacity: 0,
-    stagger: 0.1,
+    stagger: 0.075,
+    duration: 0.2,
+    ease: "power1.out",
+  });
+  tl.from('.hero-section .btn', {
+    y: 50,
+    opacity: 0,
+    stagger: 0.25,
+    duration: 0.25,
+    ease: "power1.out",
+  });
+  tl.from(".hero-section .btn svg", {
+    scale: 0,
+    y: -125,
+    opacity: 0,
     ease: "power1.out",
     scrollTrigger: {
       trigger: ".hero",
       start: "top 50%",
       end: "top 0%",
       toggleActions: "play none none reverse",
-      scrub: 1,
     },
   });
-  tl.from(".hero-item", {
-    y: 50,
+  gsap.from('.hero-section .blob', {
+    y: "+=700",
+    x: 700,
     opacity: 0,
-    stagger: 0.5,
+    duration: 1,
     ease: "power1.out",
     scrollTrigger: {
       trigger: ".pin-1",
-      start: "top 0%",
-      end: "center 10%",
       toggleActions: "play none none reverse",
-      scrub: 2,
     },
   });
 }
 
 function createAchievementTimeline(tl) {
-  tl.from(".achievements", {
-    y: 250,
+  tl.play()
+  tl.from(".achievement-text", {
+    y: 20,
     opacity: 0,
-    duration: 0.4,
+    duration: 0.2,
+    stagger: 0.2,
     ease: "power1.out",
-    boxShadow: "0px 0px 0px 0 rgba(0, 0, 0, 0.5)",
     scrollTrigger: {
       trigger: ".achievements",
-      start: "top 40%",
-      end: "top 20%",
-      toggleActions: "play none none reverse",
-      scrub: 0.5,
+      start: "top 100",
+      end: "top 100%",
+      toggleActions: "play resume none resume",
     },
   });
   tl.from(".achievement-card", {
@@ -56,21 +66,9 @@ function createAchievementTimeline(tl) {
     stagger: 0.1,
     ease: "power1.out",
     scrollTrigger: {
-      trigger: ".achievements",
-      start: "top 100%",
-      end: "top 10%",
-      toggleActions: "play none none reverse",
-      scrub: 0.25,
-    },
-  });
-  tl.to(".achievements", {
-    boxShadow: "12px 12px 6px 0 rgba(0, 0, 0, 0.5)",
-    scrollTrigger: {
-      trigger: ".achievements",
-      start: "bottom 80%",
-      end: "top 10%",
-      toggleActions: "play none none reverse",
-      scrub: true,
+      trigger: ".achievement-card",
+      start: "top 2000px",
+      end: "center 10%",
     },
   });
 }
@@ -83,24 +81,17 @@ function createAboutUsTimeline(tl) {
     ease: "power1.out",
     scrollTrigger: {
       trigger: ".about-us-section",
-      start: "top +120%",
-      end: "top -20%",
-      toggleActions: "play none none reverse",
-      scrub: true,
     },
   });
-  tl.from(".about-us-header", {
-    y: 250,
-    scale: 1.5,
+  tl.from(".about-us", {
+    y: 100,
     opacity: 0,
-    stagger: 0.1,
     ease: "power1.out",
     scrollTrigger: {
-      trigger: ".about-us-section",
-      start: "top 80%",
-      end: "top -20%",
+      trigger: ".achievement",
+      start: "top top",
+      end: "top 20%",
       toggleActions: "play none none reverse",
-      scrub: 0.125,
     },
   });
   tl.from(".about-us-section .col", {
@@ -110,25 +101,24 @@ function createAboutUsTimeline(tl) {
     ease: "power1.out",
     scrollTrigger: {
       trigger: ".about-us-section",
-      start: "top top",
-      end: "bottom 100%",
+      start: "top 0",
+      end: "bottom 20%",
       toggleActions: "play none none reverse",
       scrub: 0.0625,
     },
   });
-  tl.from(".about-us-section .col svg", {
-    y: 50,
-    opacity: 0,
-    stagger: 0.2,
-    ease: "power1.out",
-    scrollTrigger: {
-      trigger: ".about-us-section .col",
-      start: "top -10%",
-      end: "bottom 40%",
-      toggleActions: "play none none reverse",
-      scrub: true,
-    },
-  });
+  // tl.from(".about-us-section .col svg", {
+  //   y: 50,
+  //   opacity: 0,
+  //   stagger: 0.2,
+  //   ease: "power1.out",
+  //   scrollTrigger: {
+  //     trigger: ".about-us-section .col",
+  //     start: "top -10%",
+  //     end: "bottom 40%",
+  //     toggleActions: "play none none reverse",
+  //   },
+  // });
 }
 
 function createTestimonialsTimeline(tl) {
@@ -220,10 +210,9 @@ function createServicesTimeline(tl) {
     ease: "power1",
     scrollTrigger: {
       trigger: ".services-section",
-      start: "top 100px",
-      end: "bottom 100%",
-      toggleActions: "play none none reverse",
-      scrub: 2,
+      start: "center 100%",
+      end: "top 0px",
+      toggleActions: "play play play complete",
     },
   });
   // tl.from(".testimonial", {
@@ -256,17 +245,10 @@ function createServicesTimeline(tl) {
 }
 
 const tl = gsap.timeline({
-  defaults: { ease: "power1.out", duration: 1, delay: 0, stagger: 0.2 },
+  defaults: { ease: "power1.out", duration: 1, delay: 0 },
 });
 createHeaderTimeline(tl);
 createHeroTimeline(tl);
-// Pin "pin-1" while achievement is scrolled
-// ScrollTrigger.create({
-//   trigger: ".pin-1",
-//   start: "top top",
-//   end: "bottom bottom",
-//   pin: ".pin-1",
-// });
 createAchievementTimeline(tl);
 createAboutUsTimeline(tl);
 createTestimonialsTimeline(tl);
