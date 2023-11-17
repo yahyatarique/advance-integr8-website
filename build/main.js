29cf6eb0a364ae6b5529638c6fba48d997a867c6
Yahya Tarique <coolyahya.03@gmail.com>
2023-11-17 18:17:12 +0530

    Added Hero animation.

--- build/main.js
+++ build/main.js
@@ -1,263 +1,263 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// function createHeaderTimeline(tl) {
-//   tl.from(".logo", {
-//     y: -50,
-//     x: -100,
-//     opacity: 0,
-//     scale: 0,
-//     ease: "power1.out",
-//     duration: 0.5,
-//   });
-//   tl.from(".nav-item", {
-//     y: -50,
-//     opacity: 0,
-//     stagger: 0.1,
-//     ease: "power1.out",
-//     duration: 0.5,
-//   });
-// }
+function createHeaderTimeline(tl) {
+  tl.from(".logo", {
+    y: -50,
+    x: -100,
+    opacity: 0,
+    scale: 0,
+    ease: "power1.out",
+    duration: 0.5,
+  });
+  tl.from(".nav-item", {
+    y: -50,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "power1.out",
+    duration: 0.5,
+  });
+}
 
-// function createHeroTimeline(tl) {
-//   tl.from(".hero-item", {
-//     y: 20,
-//     opacity: 0,
-//     stagger: 0.075,
-//     duration: 0.2,
-//     ease: "power1.out",
-//   });
-//   tl.from(".hero-section .btn", {
-//     y: 50,
-//     opacity: 0,
-//     stagger: 0.25,
-//     duration: 0.25,
-//     ease: "power1.out",
-//   });
-//   tl.from(".hero-section .btn svg", {
-//     scale: 0,
-//     y: -125,
-//     opacity: 0,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".hero",
-//       start: "top 50%",
-//       end: "top 0%",
-//       toggleActions: "play none none reverse",
-//     },
-//   });
-//   // gsap.from(".hero-section .blob", {
-//   //   y: "+=700",
-//   //   x: 700,
-//   //   opacity: 0,
-//   //   duration: 1,
-//   //   ease: "power1.out",
-//   //   scrollTrigger: {
-//   //     trigger: ".pin-1",
-//   //     toggleActions: "play none none reverse",
-//   //   },
-//   // });
-// }
+function createHeroTimeline(tl) {
+  tl.from(".hero-item", {
+    y: 20,
+    opacity: 0,
+    stagger: 0.075,
+    duration: 0.2,
+    ease: "power1.out",
+  });
+  tl.from(".hero-section .btn", {
+    y: 50,
+    opacity: 0,
+    stagger: 0.25,
+    duration: 0.25,
+    ease: "power1.out",
+  });
+  tl.from(".hero-section .btn svg", {
+    scale: 0,
+    y: -125,
+    opacity: 0,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "top 50%",
+      end: "top 0%",
+      toggleActions: "play none none reverse",
+    },
+  });
+  // gsap.from(".hero-section .blob", {
+  //   y: "+=700",
+  //   x: 700,
+  //   opacity: 0,
+  //   duration: 1,
+  //   ease: "power1.out",
+  //   scrollTrigger: {
+  //     trigger: ".pin-1",
+  //     toggleActions: "play none none reverse",
+  //   },
+  // });
+}
 
-// function createAchievementTimeline(tl) {
-//   tl.play();
-//   tl.from(".achievement-text", {
-//     y: 20,
-//     opacity: 0,
-//     duration: 0.2,
-//     stagger: 0.2,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".achievements",
-//       start: "top 100",
-//       end: "top 100%",
-//       toggleActions: "play resume none resume",
-//     },
-//   });
-//   tl.from(".achievement-card", {
-//     y: 500,
-//     opacity: 0,
-//     stagger: 0.1,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".achievement-card",
-//       start: "top 2000px",
-//       end: "center 10%",
-//     },
-//   });
-// }
+function createAchievementTimeline(tl) {
+  tl.play();
+  tl.from(".achievement-text", {
+    y: 20,
+    opacity: 0,
+    duration: 0.2,
+    stagger: 0.2,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".achievements",
+      start: "top 100",
+      end: "top 100%",
+      toggleActions: "play resume none resume",
+    },
+  });
+  tl.from(".achievement-card", {
+    y: 500,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".achievement-card",
+      start: "top 2000px",
+      end: "center 10%",
+    },
+  });
+}
 
-// function createAboutUsTimeline(tl) {
-//   tl.from(".about-us-section > img:nth-child(1)", {
-//     y: 250,
-//     scale: 1.5,
-//     opacity: 0,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".about-us-section",
-//       start: "top 2000px",
-//       end: "center 10%",
-//     },
-//   });
-//   tl.from(".about-us-header", {
-//     y: 100,
-//     opacity: 0,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".achievement",
-//       start: "top 2000px",
-//       end: "center 10%",
-//     },
-//   });
-//   tl.from(".about-us-section .col", {
-//     y: 500,
-//     opacity: 0,
-//     stagger: 0.5,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".about-us-card",
-//       start: "top 100%",
-//       end: "bottom 20%",
-//     },
-//   });
-//   // tl.from(".about-us-section .col svg", {
-//   //   y: 50,
-//   //   opacity: 0,
-//   //   stagger: 0.2,
-//   //   ease: "power1.out",
-//   //   scrollTrigger: {
-//   //     trigger: ".about-us-section .col",
-//   //     start: "top -10%",
-//   //     end: "bottom 40%",
-//   //     toggleActions: "play none none reverse",
-//   //   },
-//   // });
-// }
+function createAboutUsTimeline(tl) {
+  tl.from(".about-us-section > img:nth-child(1)", {
+    y: 250,
+    scale: 1.5,
+    opacity: 0,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".about-us-section",
+      start: "top 2000px",
+      end: "center 10%",
+    },
+  });
+  tl.from(".about-us-header", {
+    y: 100,
+    opacity: 0,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".achievement",
+      start: "top 2000px",
+      end: "center 10%",
+    },
+  });
+  tl.from(".about-us-section .col", {
+    y: 500,
+    opacity: 0,
+    stagger: 0.5,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".about-us-card",
+      start: "top 100%",
+      end: "bottom 20%",
+    },
+  });
+  // tl.from(".about-us-section .col svg", {
+  //   y: 50,
+  //   opacity: 0,
+  //   stagger: 0.2,
+  //   ease: "power1.out",
+  //   scrollTrigger: {
+  //     trigger: ".about-us-section .col",
+  //     start: "top -10%",
+  //     end: "bottom 40%",
+  //     toggleActions: "play none none reverse",
+  //   },
+  // });
+}
 
-// function createTestimonialsTimeline(tl) {
-//   tl.from(".testimonial-header .col", {
-//     y: 250,
-//     opacity: 0,
-//     stagger: 0.1,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".testimonial-section",
-//       start: "top 80%",
-//       end: "center 20%",
-//       toggleActions: "play none none reverse",
-//       scrub: true,
-//     },
-//   });
-//   tl.from(".testimonial-image", {
-//     y: 500,
-//     opacity: 0,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".testimonial-section",
-//       start: "top top",
-//       end: "bottom 100%",
-//     },
-//   });
-//   tl.from(".testimonial", {
-//     borderBottom: "1px solid transparent",
-//     y: 500,
-//     opacity: 0,
-//     stagger: 0.2,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".testimonial-section",
-//       start: "top 50%",
-//       end: "bottom 100%",
-//       toggleActions: "play none none reverse",
-//     },
-//   });
-//   tl.from(".testimonial-details .col", {
-//     y: 500,
-//     opacity: 0,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".testimonial-section",
-//       start: "top top",
-//       end: "bottom 120%",
-//       toggleActions: "play none none reverse",
-//       scrub: true,
-//     },
-//   });
-//   tl.from(".slider-control .slider-button", {
-//     y: 2000,
-//     opacity: 0,
-//     // stagger: 0.5,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".testimonial-section",
-//       start: "top 100%",
-//       end: "bottom 100%",
-//       toggleActions: "play none none reverse",
-//       scrub: true,
-//     },
-//   });
-// }
-// function createServicesTimeline(tl) {
-//   tl.from(".services-section .row .col", {
-//     y: 250,
-//     opacity: 0,
-//     stagger: 0.5,
-//     ease: "power1.out",
-//     scrollTrigger: {
-//       trigger: ".services-section",
-//       start: "top 100%",
-//       end: "center center",
-//       toggleActions: "play none none reverse",
-//       scrub: true,
-//     },
-//   });
-//   tl.from(".services-card", {
-//     // y: 5000,
-//     x: -500,
-//     // scale: 1.2,
-//     opacity: 0,
-//     stagger: 0.4,
-//     ease: "power1",
-//     scrollTrigger: {
-//       trigger: ".services-section",
-//       start: "center 100%",
-//       end: "top 0px",
-//       toggleActions: "play play play complete",
-//     },
-//   });
-//   // tl.from(".testimonial", {
-//   //   borderBottom: "1px solid transparent",
-//   //   y: 500,
-//   //   opacity: 0,
-//   //   stagger: 0.2,
-//   //   ease: "power1.out",
-//   //   scrollTrigger: {
-//   //     trigger: ".testimonial-section",
-//   //     start: "top 50%",
-//   //     end: "bottom 100%",
-//   //     toggleActions: "play none none reverse",
-//   //     scrub: true,
-//   //   }
-//   // });
-//   // tl.from(".testimonial-details .col", {
-//   //   y: 200,
-//   //   opacity: 0,
-//   //   stagger: 0.25,
-//   //   ease: "power1.out",
-//   //   scrollTrigger: {
-//   //     trigger: ".testimonial-section",
-//   //     start: "top top",
-//   //     end: "bottom 100%",
-//   //     toggleActions: "play none none reverse",
-//   //     scrub: true,
-//   //   }
-//   // })
-// }
+function createTestimonialsTimeline(tl) {
+  tl.from(".testimonial-header .col", {
+    y: 250,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".testimonial-section",
+      start: "top 80%",
+      end: "center 20%",
+      toggleActions: "play none none reverse",
+      scrub: true,
+    },
+  });
+  tl.from(".testimonial-image", {
+    y: 500,
+    opacity: 0,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".testimonial-section",
+      start: "top top",
+      end: "bottom 100%",
+    },
+  });
+  tl.from(".testimonial", {
+    borderBottom: "1px solid transparent",
+    y: 500,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".testimonial-section",
+      start: "top 50%",
+      end: "bottom 100%",
+      toggleActions: "play none none reverse",
+    },
+  });
+  tl.from(".testimonial-details .col", {
+    y: 500,
+    opacity: 0,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".testimonial-section",
+      start: "top top",
+      end: "bottom 120%",
+      toggleActions: "play none none reverse",
+      scrub: true,
+    },
+  });
+  tl.from(".slider-control .slider-button", {
+    y: 2000,
+    opacity: 0,
+    // stagger: 0.5,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".testimonial-section",
+      start: "top 100%",
+      end: "bottom 100%",
+      toggleActions: "play none none reverse",
+      scrub: true,
+    },
+  });
+}
+function createServicesTimeline(tl) {
+  tl.from(".services-section .row .col", {
+    y: 250,
+    opacity: 0,
+    stagger: 0.5,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".services-section",
+      start: "top 100%",
+      end: "center center",
+      toggleActions: "play none none reverse",
+      scrub: true,
+    },
+  });
+  tl.from(".services-card", {
+    // y: 5000,
+    x: -500,
+    // scale: 1.2,
+    opacity: 0,
+    stagger: 0.4,
+    ease: "power1",
+    scrollTrigger: {
+      trigger: ".services-section",
+      start: "center 100%",
+      end: "top 0px",
+      toggleActions: "play play play complete",
+    },
+  });
+  // tl.from(".testimonial", {
+  //   borderBottom: "1px solid transparent",
+  //   y: 500,
+  //   opacity: 0,
+  //   stagger: 0.2,
+  //   ease: "power1.out",
+  //   scrollTrigger: {
+  //     trigger: ".testimonial-section",
+  //     start: "top 50%",
+  //     end: "bottom 100%",
+  //     toggleActions: "play none none reverse",
+  //     scrub: true,
+  //   }
+  // });
+  // tl.from(".testimonial-details .col", {
+  //   y: 200,
+  //   opacity: 0,
+  //   stagger: 0.25,
+  //   ease: "power1.out",
+  //   scrollTrigger: {
+  //     trigger: ".testimonial-section",
+  //     start: "top top",
+  //     end: "bottom 100%",
+  //     toggleActions: "play none none reverse",
+  //     scrub: true,
+  //   }
+  // })
+}
 
-// const tl = gsap.timeline({
-//   defaults: { ease: "power1.out", duration: 1, delay: 0 },
-// });
-// createHeaderTimeline(tl);
-// createHeroTimeline(tl);
+const tl = gsap.timeline({
+  defaults: { ease: "power1.out", duration: 1, delay: 0 },
+});
+createHeaderTimeline(tl);
+createHeroTimeline(tl);
 // createAchievementTimeline(tl);
 // createAboutUsTimeline(tl);
 // createTestimonialsTimeline(tl);
